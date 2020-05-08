b8dcb4583ba6fef58b5ed17f80b261d04ac4c050
KaiWei Shen <keshen@ucdavis.edu>
2020-05-07 21:48:04 -0700

    my side code

--- script.js
+++ script.js
@@ -44,3 +44,25 @@ var settings = {
 $.ajax(settings).done(function (response) {
 	console.log(response);
 });
+
+var searchEntry = "Apple";
+var urbanAPI = {
+"async": true,
+"crossDomain": true,
+"url": "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=" + searchEntry,
+"method": "GET",
+"headers": {
+    "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
+    "x-rapidapi-key": "7ac5693631msh1be654ee56cba4ap14b22cjsn1aef1fe207ca"
+}
+}
+
+$.ajax(urbanAPI).done(function (response) {
+    
+console.log(response);
+
+var searchTest = $("<text>")
+searchTest.text("");
+searchTest.text(JSON.stringify(response.data[i]));
+    
+});
