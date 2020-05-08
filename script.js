df51387ba49841a6d4916ff42e27a82e7688929d
KaiWei Shen <keshen@ucdavis.edu>
2020-05-07 19:29:15 -0700

    urban

--- script.js
+++ script.js
@@ -29,3 +29,18 @@ var geniusApi = {
 $.ajax(geniusApi).done(function(response) {
     console.log(response);
 });
+
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=wat",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
+		"x-rapidapi-key": "7ac5693631msh1be654ee56cba4ap14b22cjsn1aef1fe207ca"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
