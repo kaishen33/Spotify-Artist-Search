9d645f3a668a63cc7525374750efb61bc58c9591
IPv5 <donationbuycraft@gmail.com>
2020-05-09 10:04:33 -0700

    Changed variable named and cleaned up code

--- script.js
+++ script.js
@@ -1,77 +1,73 @@
 //GLOBAL VAR
 
-$("#find-music").on("click",function(event){
+$("#findMusicBtn").on("click", function(event) {
     event.preventDefault();
-    $(".forecast1").empty();
+    $(".songInfoClass").empty();
 
     var i = 0
-    var artistName =$("#music-input").val();
+    var artistName = $("#music-input").val();
 
-    //This is DEEZER api call
+    //Grabbing the songs by artist name
     var deezerApi = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artistName,
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-        "x-rapidapi-key": "d8bb7331d7mshcb58195d90da480p15ca06jsn0559dad38ac9"
-    }
-}
-    
-    $.ajax(deezerApi).done(function(response) {
-        console.log(response.data[0].title);
-    
-        $("#button1").on("click",function(event){
-            $(".workP").empty();
-            $(".forecast1").empty();
-            i++
-            var work = $("<div>");
-            var workP = $("<p>");
-            workP.text(JSON.stringify(response.data[i]));
-            var songName = (response.data[i].title);
-            console.log(songName);
-
-        workP.attr("Class", "workP");
-        work.attr('class','work');
-        //($(".work")).append(workP);
-      ($(".forecast1")).append(workP);
-
-
-      var lyricsApi = {
         "async": true,
         "crossDomain": true,
-        "url": "https://canarado-lyrics.p.rapidapi.com/lyrics/" + songName + artistName,
+        "url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artistName,
         "method": "GET",
         "headers": {
-            "x-rapidapi-host": "canarado-lyrics.p.rapidapi.com",
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
             "x-rapidapi-key": "d8bb7331d7mshcb58195d90da480p15ca06jsn0559dad38ac9"
         }
     }
-    
-    $.ajax(lyricsApi).done(function(response) {
-        console.log(response);
-    
-        function displayArtistInfo() {
-            var displayArtistName = response.content[0].artist;
-            var displaySongTitle = response.content[0].title;
-            $("#artistName").text(displayArtistName);
-            $("#artistSongName").text(displaySongTitle);
-        }
-        displayArtistInfo();
-    
-        function displaySongLyrics() {
-            var songLyrics = response.content[0].lyrics;
-            $("#lyricsP").text(songLyrics);
-        }
-        displaySongLyrics();
-    
-    
-    
-    });
-            
+    $.ajax(deezerApi).done(function(response) {
+        console.log(response.data[0].title);
+
+        $("#nextSongBtn").on("click", function() {
+            $(".songP").empty();
+            $(".songInfoClass").empty();
+            i++;
+            var songDiv = $("<div>");
+            var songP = $("<p>");
+            songP.text(JSON.stringify(response.data[i]));
+            var songName = (response.data[i].title);
+            console.log(songName);
+
+            songP.attr("Class", "songP");
+            songDiv.attr('class', 'songDiv');
+            $(".songInfoClass").append(songP);
+
+
+            //Grabbing lyrics of the song
+            var lyricsApi = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://canarado-lyrics.p.rapidapi.com/lyrics/" + songName + artistName,
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "canarado-lyrics.p.rapidapi.com",
+                    "x-rapidapi-key": "d8bb7331d7mshcb58195d90da480p15ca06jsn0559dad38ac9"
+                }
+            }
+            $.ajax(lyricsApi).done(function(response) {
+                console.log(response);
+                displayArtistInfo();
+                displaySongLyrics();
+
+                function displayArtistInfo() {
+                    var displayArtistName = response.content[0].artist;
+                    var displaySongTitle = response.content[0].title;
+                    $("#artistName").text(displayArtistName);
+                    $("#artistSongName").text(displaySongTitle);
+                }
+
+                function displaySongLyrics() {
+                    var songLyrics = response.content[0].lyrics;
+                    $("#lyricsP").text(songLyrics);
+                }
+
+            });
+
         });
-    
-      
-});
+
+
+    });
 });
